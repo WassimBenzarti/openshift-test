4111a0437f8b6d7c60d67e7673804624e2dd63c4
Guetta benz <Guetta benz>
2016-05-09 15:14:11 +0100

    aaaze

--- php/connect.php
+++ php/connect.php
@@ -1,19 +1,12 @@
 <?php
 ignore_user_abort(true);
 define('databaseName','wassim');
-define('databaseData',array(
-        'url' => $_SERVER["OPENSHIFT_MYSQL_DB_HOST"],
-        'port' => $_SERVER["OPENSHIFT_MYSQL_DB_PORT"],
-        'user' => $_SERVER["OPENSHIFT_MYSQL_DB_USERNAME"],
-        'pass' => $_SERVER["OPENSHIFT_MYSQL_DB_PASSWORD"]
-));
-echo var_dump(databaseData);
 define('SERVERROOTPATH',dirname(dirname(__FILE__)));
 define('SERVERMENUPATH',SERVERROOTPATH."/menu.php");
 define('SERVERFOOTERPATH',SERVERROOTPATH."/footer.php");
 define('SERVERHEADPATH',SERVERROOTPATH."/inc/common/head.php");
 define('FBTOKEN',"CAAXnZBDdib2sBAI7NuQogPZCJUstQgTRQiVjoBSzZBZCwoBkY4kH7SdvlYRmxICWZCyO47SBAc5ZAU3xhpsjIO0oeWzP8ofledmrESFlWIrGcvDLWjcxjN2ZAEr93JAtJuEuuRseU2NQ7L7myspWLhrQuutBZBXR4oUY5l38tnXsschWHvgNY9B4Wwm80qnQzDIZD");
-$db = new PDO('mysql:host=127.13.162.130:3306;dbname='.databaseName,'adminpjV2Bj9','TdeU3Y7mtfQH');
+$db = new PDO("mysql:host=".$_SERVER["OPENSHIFT_MYSQL_DB_HOST"].":".$_SERVER["OPENSHIFT_MYSQL_DB_PORT"].";dbname=".databaseName,$_SERVER["OPENSHIFT_MYSQL_DB_USERNAME"],$_SERVER["OPENSHIFT_MYSQL_DB_PASSWORD"]);
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
 ?>
