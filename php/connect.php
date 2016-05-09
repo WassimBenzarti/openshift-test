ef55943fe7968c8766f7f9235bad2816bce30db5
Guetta benz <Guetta benz>
2016-05-09 12:54:28 +0100

    hello

--- php/connect.php
+++ php/connect.php
@@ -9,6 +9,5 @@ define('SERVERHEADPATH',SERVERROOTPATH."/inc/common/head.php");
 define('FBTOKEN',"CAAXnZBDdib2sBAI7NuQogPZCJUstQgTRQiVjoBSzZBZCwoBkY4kH7SdvlYRmxICWZCyO47SBAc5ZAU3xhpsjIO0oeWzP8ofledmrESFlWIrGcvDLWjcxjN2ZAEr93JAtJuEuuRseU2NQ7L7myspWLhrQuutBZBXR4oUY5l38tnXsschWHvgNY9B4Wwm80qnQzDIZD");
 $db = new PDO('mysql:host=127.13.162.130:3306;dbname='.databaseName,'adminpjV2Bj9','TdeU3Y7mtfQH');
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-echo "connected";
 
 ?>
