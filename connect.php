178068b7230852ac007b21f6b2d1ac9bd9c1b6d2
Guetta benz <Guetta benz>
2016-05-09 11:46:35 +0100

    connect uploaded

--- connect.php
+++ connect.php
@@ -8,6 +8,6 @@ define('SERVERHEADPATH',SERVERROOTPATH."/inc/common/head.php");
 define('FBTOKEN',"CAAXnZBDdib2sBAI7NuQogPZCJUstQgTRQiVjoBSzZBZCwoBkY4kH7SdvlYRmxICWZCyO47SBAc5ZAU3xhpsjIO0oeWzP8ofledmrESFlWIrGcvDLWjcxjN2ZAEr93JAtJuEuuRseU2NQ7L7myspWLhrQuutBZBXR4oUY5l38tnXsschWHvgNY9B4Wwm80qnQzDIZD");
 $db = new PDO('mysql:host=127.0.0.1;dbname='.databaseName,'root','') or die("error connecting!");
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
+echo "connected";
 
 ?>
