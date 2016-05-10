106cca8db65c7636bdb637338d195ee9572fc807
Guetta benz <Guetta benz>
2016-05-10 11:54:25 +0100

    shelloWorld

--- php/connect.php
+++ php/connect.php
@@ -1,14 +1,12 @@
 <?php
 ignore_user_abort(true);
 define('databaseName','wassim');
-define('SERVERROOTPATH',getenv("OPENSHIFT_REPO_DIR")."/php");
-
+define('SERVERROOTPATH',getenv("OPENSHIFT_REPO_DIR")."php");
+define('SERVERDATAPATH',getenv("OPENSHIFT_DATA_DIR"));
 define('SERVERMENUPATH',SERVERROOTPATH."/menu.php");
 define('SERVERFOOTERPATH',SERVERROOTPATH."/footer.php");
 define('SERVERHEADPATH',SERVERROOTPATH."/inc/common/head.php");
-echo SERVERMENUPATH."<br>";
-echo SERVERFOOTERPATH."<br>";
-echo SERVERHEADPATH."<br>";
+
 define('FBTOKEN',"CAAXnZBDdib2sBAI7NuQogPZCJUstQgTRQiVjoBSzZBZCwoBkY4kH7SdvlYRmxICWZCyO47SBAc5ZAU3xhpsjIO0oeWzP8ofledmrESFlWIrGcvDLWjcxjN2ZAEr93JAtJuEuuRseU2NQ7L7myspWLhrQuutBZBXR4oUY5l38tnXsschWHvgNY9B4Wwm80qnQzDIZD");
 $db = new PDO("mysql:host=".getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT").";dbname=".databaseName,getenv("OPENSHIFT_MYSQL_DB_USERNAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
