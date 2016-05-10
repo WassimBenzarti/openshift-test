ad1b06c0e1d63bf130d4cc7a2f4ddbf48ed5b93e
Guetta benz <Guetta benz>
2016-05-10 01:40:25 +0100

    shelloWorld

--- php/index.php
+++ php/index.php
@@ -1,3 +1,17 @@
-<?php
-  phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+    <style>
+      h1{
+        position:absolute;
+        top:50%; left:50%;
+        transform:translate(-50%,-50%);
+      }
+    </style>
+  </head>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>
