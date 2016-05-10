4dedec7fe2244af05b32e1ec33c602b04938aecd
Guetta benz <Guetta benz>
2016-05-10 21:22:15 +0100

    shelloWorld

--- php/head.php
+++ php/head.php
@@ -1,24 +1,24 @@
 <meta charset="utf-8">
-<link rel="shortcut icon" href="/inc/favicon/favicon.ico">
-<link rel="icon" sizes="16x16 32x32 64x64" href="/inc/favicon/favicon.ico">
-<link rel="icon" type="image/png" sizes="196x196" href="/inc/favicon/favicon-192.png">
-<link rel="icon" type="image/png" sizes="160x160" href="/inc/favicon/favicon-160.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/inc/favicon/favicon-96.png">
-<link rel="icon" type="image/png" sizes="64x64" href="/inc/favicon/favicon-64.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/inc/favicon/favicon-32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/inc/favicon/favicon-16.png">
-<link rel="apple-touch-icon" href="/inc/favicon/favicon-57.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/inc/favicon/favicon-114.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/inc/favicon/favicon-72.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/inc/favicon/favicon-144.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/inc/favicon/favicon-60.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/inc/favicon/favicon-120.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/inc/favicon/favicon-76.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/inc/favicon/favicon-152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/inc/favicon/favicon-180.png">
+<link rel="shortcut icon" href="/src/inc/favicon/favicon.ico">
+<link rel="icon" sizes="16x16 32x32 64x64" href="/src/inc/favicon/favicon.ico">
+<link rel="icon" type="image/png" sizes="196x196" href="/src/inc/favicon/favicon-192.png">
+<link rel="icon" type="image/png" sizes="160x160" href="/src/inc/favicon/favicon-160.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/src/inc/favicon/favicon-96.png">
+<link rel="icon" type="image/png" sizes="64x64" href="/src/inc/favicon/favicon-64.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/src/inc/favicon/favicon-32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/src/inc/favicon/favicon-16.png">
+<link rel="apple-touch-icon" href="/src/inc/favicon/favicon-57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/src/inc/favicon/favicon-114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/src/inc/favicon/favicon-72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/src/inc/favicon/favicon-144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/src/inc/favicon/favicon-60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/src/inc/favicon/favicon-120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/src/inc/favicon/favicon-76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/src/inc/favicon/favicon-152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/src/inc/favicon/favicon-180.png">
 <meta name="msapplication-TileColor" content="#FFFFFF">
-<meta name="msapplication-TileImage" content="/inc/favicon/favicon-144.png">
-<meta name="msapplication-config" content="/inc/favicon/browserconfig.xml">
+<meta name="msapplication-TileImage" content="/src/inc/favicon/favicon-144.png">
+<meta name="msapplication-config" content="/src/inc/favicon/browserconfig.xml">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <script src="/js/jquery.history.js"></script>
 <script src="/js/jquery.easing.min.js"></script>
