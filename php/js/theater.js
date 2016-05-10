9b7829b1d579f3841116f371ff0f53af5f19ff1d
Guetta benz <Guetta benz>
2016-05-10 22:58:44 +0100

    shelloWorld

--- php/js/theater.js
+++ php/js/theater.js
@@ -45,8 +45,8 @@
             //offX= cPt(self.original)[0] - ($(window).outerWidth()/2) ,
             //offY = cPt(self.original)[1] - ($(window).outerHeight()/2);
             //offScale = self.original.width()/self.elm.width(),
-            offX = self.original.offset().left - (self.elm.width()*offScale)/2 - ($(window).outerWidth()/2),
-            offY = self.original.offset().top - (self.elm.height()*offScale)/2 - ($(window).outerHeight()/2);
+            offX = cPt(self.original)[0] - (self.elm.width()*offScale)/2 - ($(window).outerWidth()/2),
+            offY = cPt(self.original)[1] - (self.elm.height()*offScale)/2 - ($(window).outerHeight()/2);
 
         setVendor(elm.get(0),'Transform','translate3d('+offX+'px,'+offY+'px,0px) scale('+offScale+')');
 
