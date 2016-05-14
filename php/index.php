65fc4474a16f7b242a42e34822c6931c25b8245f
Guetta benz <Guetta benz>
2016-05-14 16:52:46 +0100

    shelloWorld

--- php/index.php
+++ php/index.php
@@ -2,5 +2,6 @@
 //define('SERVERROOTPATH',);
 //echo $_SERVER['DOCUMENT_ROOT'];
 //echo SERVERROOTPATH;
-echo (empty(getenv("OPENSHIFT_REPO_DIR")))?$_SERVER["DOCUMENT_ROOT"]:getenv("OPENSHIFT_REPO_DIR")."php1";
+
+echo getenv("OPENSHIFT_REPO_DIR");
 ?>
