cb314ff31d201dda53c45b0f8e372d01f5711f29
Guetta benz <Guetta benz>
2016-05-14 15:49:50 +0100

    shelloWorld

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,5 @@
 <?php
 ignore_user_abort(true);
-define('databaseName','wassim');
 define('SERVERROOTPATH',getenv("OPENSHIFT_REPO_DIR")."php");
 define('SERVERDATAPATH',getenv("OPENSHIFT_DATA_DIR"));
 define('SERVERMENUPATH',SERVERROOTPATH."/menu.php");
@@ -8,6 +7,11 @@ define('SERVERFOOTERPATH',SERVERROOTPATH."/footer.php");
 define('SERVERHEADPATH',SERVERROOTPATH."/head.php");
 
 define('FBTOKEN',"EAAEPz5DjZAhUBANRkWM5qhdzV6mppteUPd57upnpsVEMbvZCp4302MsER7u18DePlF9jl76YZB3NOHUbA7WjLMWRoPU6CRKjRbUanV0szIgX8LI9MHrbj2S3XZCdiRTxjVDTQQBkZBXXQwFq4mzDI65skXPPm8LTsEch7KuA61AZDZD");
-$db = new PDO("mysql:host=".getenv("OPENSHIFT_MYSQL_DB_HOST").":".getenv("OPENSHIFT_MYSQL_DB_PORT").";dbname=".databaseName,getenv("OPENSHIFT_MYSQL_DB_USERNAME"),getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
+$db_host = getenv("OPENSHIFT_MYSQL_DB_HOST");
+$db_port = getenv("OPENSHIFT_MYSQL_DB_PORT");
+$db_name = 'wassim';
+$db_user = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+$db_pass = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+$db = new PDO("mysql:host=".$db_host.":".$db_port.";dbname=".$db_name,$db_user,$db_pass);
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 ?>
