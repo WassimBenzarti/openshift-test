6560a79717c15f8829aa451bb907c3773893cb99
Guetta benz <Guetta benz>
2016-05-14 17:36:01 +0100

    shelloWorld

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,7 @@
 <?php
 ignore_user_abort(true);
-define('SERVERROOTPATH',(getenv("OPENSHIFT_REPO_DIR")==false) ? $_SERVER['DOCUMENT_ROOT'] : getenv("OPENSHIFT_REPO_DIR")."php");
-
-define('SERVERDATAPATH',(getenv("OPENSHIFT_DATA_DIR")==false) ? $_SERVER['DOCUMENT_ROOT']."src" : SERVERROOTPATH."/src");
+define('SERVERROOTPATH',getenv("OPENSHIFT_REPO_DIR")."/php");
+define('SERVERDATAPATH',SERVERROOTPATH."/src");
 define('SERVERMENUPATH',SERVERROOTPATH."/menu.php");
 define('SERVERFOOTERPATH',SERVERROOTPATH."/footer.php");
 define('SERVERHEADPATH',SERVERROOTPATH."/head.php");
@@ -13,5 +12,6 @@ $db_name = 'wassim';
 $db_user = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
 $db_pass = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
 $db = new PDO("mysql:host=".$db_host.":".$db_port.";dbname=".$db_name,$db_user,$db_pass);
+
 $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 ?>
--- php/index.php
+++ php/index.php
@@ -1,6 +1,64 @@
 <?php
-require_once("connect.php");
-
-echo SERVERROOTPATH."<br>";
-echo SERVERDATAPATH;
+  require_once(getenv("OPENSHIFT_REPO_DIR")."php/connect.php");
 ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CoDesign</title>
+    <?php require_once(SERVERHEADPATH); ?>
+  </head>
+
+  <body id="body" class="loading" bgcolor="#1B1B1B">
+    <?php require_once(SERVERMENUPATH); ?>
+    <div id="mainContainer">
+      <link rel="stylesheet" href="css/index.css" charset="utf-8">
+      <script src="/js/firewall.min.js"></script>
+      <script src="/js/imageLoader.js"></script>
+      <script src="/js/soundcloud.js"></script>
+      <script src="/js/projectLoader.js"></script>
+      <script src="/js/loadingCircle.js"></script>
+      <script src="/js/index.js"></script>
+
+      <section class="section1">
+        <video id="liveBackground" style="display:none" preload="none" muted loop>
+          <source src="/src/Design/main-video.webm" type="video/webm">
+          <source src="/src/Design/main-video.mp4" type="video/mp4">
+        </video>
+        <div class="logo"></div>
+        <center>
+          <div class="titleLogo"></div>
+          <h2>&nbsp;WHEN CODE & DESIGN MEET TOGETHER</h2>
+        </center>
+      </section>
+      <section class="section2">
+      </section>
+      <section class="section3">
+        <h1>SOME OF MY ARTWORKS</h1>
+        <div id="firewallContainer" class="freewall"> </div>
+        <div data-number=9 class="loadMoreBtn">
+          <span onclick="artImages.loadMoreClick(this)">
+            MORE
+          </span>
+        </div>
+      </section>
+      <section class="section4">
+        <h1>SOME OF THE MUSIC I LIKE</h1>
+        <div id="musicList" class="freewall">
+
+        </div>
+        <div class="loadMoreBtn">
+          <span onclick="SC.getTracks(true)">
+            MORE
+          </span>
+        </div>
+      </section>
+      <section class="PROJECTS">
+        <h1>PROJECTS I MADE</h1>
+        <div id="projectList">
+
+        </div>
+        <a href="/projects/" class="link">SEE MORE OF MY PROJECTS</a>
+      </section>
+    </div>
+
+<?php require_once(SERVERFOOTERPATH); ?>
