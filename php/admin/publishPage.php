fb2a85910941663c82ea54f839eef4067008a1df
Guetta benz <Guetta benz>
2016-05-23 21:49:27 +0100

    shelloWorld

--- php/admin/publishPage.php
+++ php/admin/publishPage.php
@@ -1,11 +1,21 @@
 <?php
-require_once(getenv("OPENSHIFT_REPO_DIR")."php/connect.php");
+  require_once(getenv("OPENSHIFT_REPO_DIR")."php/connect.php");
+  $old = $db->prepare("UPDATE facebookpics SET done = 1 WHERE id = :id");
+
+  date_default_timezone_set('Africa/Tunis');
+  $date = date("Y-m-d H:i:s");echo $date;
+  $get = $db->prepare("SELECT * FROM facebookpics WHERE date < '$date' AND done = 0 LIMIT 1");
+  $get->execute();
+  $img = $get->fetchAll();
+  echo "<pre>";
+  var_dump($img);
+  echo "</pre>";
   ini_set("memory_limit",-1);
   define('UTOKEN','access_token=EAACoSoJeH3wBAAJpxTzTyZAFpGrktOaE6C37QfpzINj0kbDZCuocLhfJdpXet0YomQ5xQqWefBgDoAK1wa4Ts4ASmxUUWbHZB8aWmZArTmGDZByHTI8WyRHeFcrUTuHmiaIWSNKdfcy9ZCmVZCJNlZA3OMlUYZBJvkvgZD');
   define('testUTOKEN','access_token=EAAG9k5MGFPYBAIRic40ZBf6tuq7XWy8gPRUqEZAxW6VBLgMJdeGcZBLZCvXviIveAZAsbfGMgLhFKeTSIhIuCPhoE48lLelTL8tYTc4pnIfWhkSR8Wp029VVq6H24rL8AZCSEC4dScZBvjdjRrk6knHpLuZB7ZBrjGqMZD');
   define('PTOKEN','access_token=EAAEPz5DjZAhUBAC8sZBYriqZBiy8dWo3TwZCiA9ApvklbXBT8ro0lZC3pvN4gNZCKOX7JZC9e58i8KLrWMpyM6OhNTrUIaZB1vqZCCijFbA2XM2hYDZB6yjzyopMqOAp6VqPEGNLZAHmJqTo6QDwbu5ejc5eGbCYSYhMFFKUAKNejmvwwZDZD');
   define('testPTOKEN','access_token=EAAXnZBDdib2sBAFcwEZAozOKZCaUEZBQoAviQIODZCfplFgB1pcjuTRSYOzbzwfnoZBYr7HZCgP3lJdzOYTXM0WEqeF5Om4hu5xstjch2wl5yez1Nko58fAsJLAl0HxeHKswut0F9dJe2oCrkUEWzaDZBO3Wv8aogV6iAjqolQe2ggZDZD');
-  function share($url,$caption,$image){
+  function share($img,$url,$caption,$image,$old){
     if(!isset($caption)){$caption = "";}
     $data = array('caption' => $caption, 'url' =>$image);
     $options = array(
@@ -20,12 +30,15 @@ require_once(getenv("OPENSHIFT_REPO_DIR")."php/connect.php");
     $succ = 0;
     if ($result !== FALSE) { /* Handle error */
       $succ = 1;
+
+      $old->execute(['id' => $img[0]['id']]);
+
       return ['success'=>$succ];
     }
     return ['success'=>$succ];
   }
-  if(isset($_GET['upload'])){
-    $res = share("https://graph.facebook.com/me/photos?".testPTOKEN,"hello","https://www.moooi.com/sites/default/files/styles/large/public/product-images/random_detail.jpg?itok=ErJveZTY");
+  if(isset($_GET['upload']) && sizeof($img) >0){
+    $res = share($img,"https://graph.facebook.com/me/photos?".testPTOKEN,"hello","https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
     echo "<pre>";
     echo json_encode($res);
     echo "</pre>";
