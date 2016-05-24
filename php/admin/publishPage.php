e38aba96c8da25a46524cdaf5b2dabdb3c3c5225
Guetta benz <Guetta benz>
2016-05-24 16:26:53 +0100

    shelloWorld

--- php/admin/publishPage.php
+++ php/admin/publishPage.php
@@ -9,17 +9,14 @@
   $get = $db->prepare("SELECT * FROM facebookpics WHERE date BETWEEN '$date2' AND '$date' AND done = 0 LIMIT 1");
   $get->execute();
   $img = $get->fetchAll();
-  echo "<pre>";
-  var_dump($img);
-  echo "</pre>";
   ini_set("memory_limit",-1);
-  define('UTOKEN','access_token=EAACoSoJeH3wBAAJpxTzTyZAFpGrktOaE6C37QfpzINj0kbDZCuocLhfJdpXet0YomQ5xQqWefBgDoAK1wa4Ts4ASmxUUWbHZB8aWmZArTmGDZByHTI8WyRHeFcrUTuHmiaIWSNKdfcy9ZCmVZCJNlZA3OMlUYZBJvkvgZD');
+  define('UTOKEN','access_token=EAAPOFeYQ0EcBANBbWXegV957pKywE8FSykreknTxxMBCTARXPoCq3f11GNzY8T3JgETZCC5qWPZCvuYrm5nDUsadJBLrVFRNDZAFZA5QwFJZAcDyVDgiKQfHYMZAMyocW0QldKJBcbVZABQMe5huUzINC3gMlygM5gZD');
   define('testUTOKEN','access_token=EAAG9k5MGFPYBAEqt0joNXxAllDiOXV2YmGWCPZAiMmNIezsWjZCORcGqroUOvmuHlNJwyKQimJTeJYPZCZC6iYWcmFT1ayCr31qZAb4bNxu5LXSCQLJKRaVk1MtDGVLnnenfBwLGC6IUGDA63eSrCVIe857b2DdwZD');
-  define('PTOKEN','access_token=EAAEPz5DjZAhUBAC8sZBYriqZBiy8dWo3TwZCiA9ApvklbXBT8ro0lZC3pvN4gNZCKOX7JZC9e58i8KLrWMpyM6OhNTrUIaZB1vqZCCijFbA2XM2hYDZB6yjzyopMqOAp6VqPEGNLZAHmJqTo6QDwbu5ejc5eGbCYSYhMFFKUAKNejmvwwZDZD');
+  define('PTOKEN','access_token=EAAPOFeYQ0EcBAC2VB4yqvpGrtn1Ehj3roj4DtNqJ3VIFvhZBeJBc5GrQaE8x4x1TxvHAxZCXJZAZCToMAQS6gPoWXTXZBaKfepYFVMZAON4AqZBlMiYG29On3eLIZACh90JyLU6M2SKqKkiUefmrSRGim43HvPyEBDA9zZCoc1xgcBQZDZD');
   define('testPTOKEN','access_token=EAAXnZBDdib2sBAFcwEZAozOKZCaUEZBQoAviQIODZCfplFgB1pcjuTRSYOzbzwfnoZBYr7HZCgP3lJdzOYTXM0WEqeF5Om4hu5xstjch2wl5yez1Nko58fAsJLAl0HxeHKswut0F9dJe2oCrkUEWzaDZBO3Wv8aogV6iAjqolQe2ggZDZD');
-  function share($img,$url,$caption,$image,$old){
+  function share($img,$url,$image,$old){
     if(!isset($caption)){$caption = "";}
-    $data = array('caption' => $caption, 'url' =>$image,'application_id'=>'Daydream');
+    $data = array('caption' => $img[0]['caption'], 'url' =>$image);
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/x-www-form-urlencoded",
@@ -27,19 +24,29 @@
             'content' => http_build_query($data)
         )
     );
+    echo "<pre>";
+    echo var_dump($img);
+    echo "<pre>";
     $context  = stream_context_create($options);
     $result = file_get_contents($url, false, $context);
-    echo "<pre>".var_dump($result)."</pre>";
     $succ = ($result !== FALSE);
+    echo "<br>URL = ".$image;
     if($result !== FALSE){
-      $old->execute(['id' => json_encode($result)['id']]);
+      $result = json_decode($result);
+      $old->execute(['id' => $img[0]['id']]);
+      echo "<br>SUCCESS : ".$result->id;
+    }else{
+      echo "<br>ERROR : ".var_dump($result);
     }
+
     return ['success'=>$succ,'result'=>$result];
   }
   if( sizeof($img) >0){
-    $res = share($img,"https://graph.facebook.com/me/photos?".testUTOKEN,$img[0]['caption'],"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
-    echo $res['success'];
+    $res = share($img,"https://graph.facebook.com/me/photos?".PTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
+    $res = share($img,"https://graph.facebook.com/me/photos?".UTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
     die();
+  }else{
+    echo "<br>NOTHING is here";
   }
   ini_restore("memory_limit");
   ?>
