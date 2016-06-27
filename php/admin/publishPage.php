919298f12987aa74ff9bfae37711ca67fce14243
Wassim Benzart <wass11121996@gmail.com>
2016-06-27 02:50:10 +0100

    s

--- php/admin/publishPage.php
+++ php/admin/publishPage.php
@@ -48,11 +48,11 @@
   }
   if( sizeof($img) >0){
 
-    $res = share($img,"https://graph.facebook.com/me/photos?".testPTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
+    $res = share($img,"https://graph.facebook.com/me/photos?".PTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
     $img[0]['caption'] .= "
 
     Feel free to like my facebook page : https://www.fb.com/art.daydream/";
-    //$res = share($img,"https://graph.facebook.com/me/photos?".UTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
+    $res = share($img,"https://graph.facebook.com/me/photos?".UTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
     die();
   }else{
     echo "<br>NOTHING is here";
