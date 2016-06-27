711c3d9f83aac164ddfedfd9be6a96d63aac35f5
Wassim Benzart <wass11121996@gmail.com>
2016-06-27 02:46:01 +0100

    s

--- php/admin/publishPage.php
+++ php/admin/publishPage.php
@@ -1,5 +1,5 @@
 <?php
-  //if(!isset($argv[1]) || !$argv[1]=="cronjob" || !isset($argv[1]) || !$argv[1]=="upload"){die();}
+  if(!isset($argv[1]) || !$argv[1]=="cronjob" || !isset($argv[1]) || !$argv[1]=="upload"){die();}
   echo "working...";
   require_once(getenv("OPENSHIFT_REPO_DIR")."php/connect.php");
   $old = $db->prepare("UPDATE facebookpics SET done = 1, posted = 1 WHERE id = :id");
@@ -47,11 +47,11 @@
     return ['success'=>$succ,'result'=>$result];
   }
   if( sizeof($img) >0){
+
+    $res = share($img,"https://graph.facebook.com/me/photos?".testPTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
     $img[0]['caption'] .= "
 
     Feel free to like my facebook page : https://www.fb.com/art.daydream/";
-    $res = share($img,"https://graph.facebook.com/me/photos?".testPTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
-
     //$res = share($img,"https://graph.facebook.com/me/photos?".UTOKEN,"https://wassim-benzarti.rhcloud.com".$img[0]['url'],$old);
     die();
   }else{
