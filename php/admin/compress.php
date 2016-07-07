6636f2564b3f831fdf56eec4a0e88112995e9ae0
Wassim Benzart <wass11121996@gmail.com>
2016-07-07 01:31:21 +0100

    s

--- php/admin/compress.php
+++ php/admin/compress.php
@@ -25,7 +25,7 @@ if(isset($_GET['URL'])){
         $image = imagecreatefromgif($filename);
       break;
   }
-  $tmpfname = tempnam(SERVERDATAPATH, 'FOO');
+  $tmpfname = tempnam(getenv('OPENSHIFT_TMP_DIR'), 'FOO');
 
   imagejpeg($image,$tmpfname,25);
 
