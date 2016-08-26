5d913ea902cbb7abe0fab6ebeda12140397b4742
Wassim Benzart <wass11121996@gmail.com>
2016-08-26 11:39:44 +0100

    hello world

--- php/admin/compress.php
+++ php/admin/compress.php
@@ -33,23 +33,22 @@ if(isset($_GET['URL'])){
   //output_image($tmpfname);
   $buffer = file_get_contents($tmpfname,FILE_USE_INCLUDE_PATH);
 
-  /* Force download dialog...
+  /* Force download dialog... */
 	header("Content-Type: application/force-download");
 	header("Content-Type: application/octet-stream");
-	header("Content-Type: application/download"); */
+	header("Content-Type: application/download"); 
 
   /* Don't allow caching... */
 	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 
-	/* Set data type, size and filename 
-//	header("Content-Type: application/octet-stream");
-  // header("Content-Type: jpeg");
+	/* Set data type, size and filename */
+	header("Content-Type: application/octet-stream");
+	
 	header("Content-Transfer-Encoding: binary");
 	header("Content-Length: " . strlen($buffer));
-	header("Content-Disposition: attachment; filename=image.jpg");*/
+	header("Content-Disposition: attachment; filename=image.jpg");
 
 	/* Send our file... */
-  header("Content-Type:jpeg");
   echo $buffer;
 }
 ini_restore("memory_limit");
